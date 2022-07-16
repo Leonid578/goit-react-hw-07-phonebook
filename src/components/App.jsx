7b75d14590e0a160a34f543fd9d95cf36bc67712
Leonid578 <Leonid.Ivanov187@gmail.com>
2022-07-16 15:10:23 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-export const App = () => {
+import { Form } from './Form/Form';
+import Title from './Title/Title';
+import style from './App.module.css';
+import ContactList from './contactList/ContactList';
+import NotContacts from './NotContacts/NotContacts';
+import { useGetAllContactsQuery } from '../server/fetchContacts';
+
+const App = () => {
+  const { data: listContacts, error, isLoading } = useGetAllContactsQuery();
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={style.conteiner}>
+      <Title text={'Phonebook'} />
+      <Form />
+
+      <Title text={'Contacts'} />
+      {error ? (
+        <NotContacts text={`Data contacts ${error.data}`} />
+      ) : isLoading ? (
+        <NotContacts text={'Wite loading now...'} />
+      ) : (
+        <ContactList contacts={listContacts} />
+      )}
     </div>
   );
 };
+export default App;
